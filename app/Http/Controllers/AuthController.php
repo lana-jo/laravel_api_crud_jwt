cc6b76e55e8ccd499f06fc6bc19615128dc045c4
jo <lanajauhar06@gmail.com>
2025-07-02 12:51:35 +0700

    add register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,13 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+// use Illuminate\Foundation\Configuration\Middleware;
 use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function __construct()
+     // Register a new user
+    public function register(Request $request)
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
     }
 
     public function login(Request $request)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api');
-    }
-
     public function index()
     {
         try {
